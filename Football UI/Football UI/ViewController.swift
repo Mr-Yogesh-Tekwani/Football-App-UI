0fbb785bb81555184f6c746f7780afab2c044725
YOGESH TEKWANI <mryogeshtekwani@gmail.com>
2023-04-01 00:43:37 -0400

    Removed ALL main storyboard references
    Removed ALL main storyboard references

---
+++ Football UI/Football UI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Football UI
+//
+//  Created by Yogesh Tekwani on 4/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
